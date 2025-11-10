3799d6fba618d53f5984376c39a5e680be776a58
baraaig <53444384+baraaig@users.noreply.github.com>
2025-11-10 23:24:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/AI-TG/', // 
+})
+
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
